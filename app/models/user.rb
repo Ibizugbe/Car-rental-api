4c48ae5b0c8ed3bcb8ce4b32b500791d29387d48
ibizugbe <abrahamibizugbe@gmail.com>
2024-04-11 16:30:08 +0100

    Update user model with validations and associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reservations
+  validates :username, presence: true, uniqueness: true
+  validates :password, :first_name, :last_name, :dob, presence: true
 end
